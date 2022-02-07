6500619977740c1cc9c4559a183bbf9a790f6d1b
laubsauger <laubsauger@gmail.com>
2022-02-06 21:23:26 -0800

    button classes

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // express server
 const fs = require('fs');
 const express = require('express');
-const {request} = require("express");
 const app = express();
 app.use(express.static('public'));
 
